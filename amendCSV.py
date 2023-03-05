ce471dfa8e1fd4cbd27b40de320c3d154b787610
jnko266 <33516559+jnko266@users.noreply.github.com>
2023-03-05 17:17:18 +0000

    add comments and minor changes

--- amendCSV.py
+++ amendCSV.py
@@ -11,21 +11,30 @@ if len(sys.argv) < 2:
 	print("Usage: amendCSV.py <filename>")
 	sys.exit(1)
 
-filename = sys.argv[1]
-
-with open(filename, 'r') as csvfile:
+# open the CSV file in read-only mode
+with open(sys.argv[1], 'r') as csvfile:
+    # read the CSV file and get the header row
     csvreader = csv.reader(csvfile)
     header = next(csvreader)
+    
+	# get the index of the sys_id column and create a new header with sys_id as the first column
     sys_id_index = header.index("sys_id")
     new_header = ["sys_id"] + [col for col in header if col != "sys_id"]
+    
+	# create a new list of rows with sys_id data as the first column
     rows = [row for row in csvreader]
     new_rows = [[row[sys_id_index]] + [col for i, col in enumerate(row) if i != sys_id_index] for row in rows]
+    
+# close the CSV file
+csvfile.close()
 
-new_filename = filename.split(".csv")[0] + "_new.csv"
-
-with open(new_filename, 'w', newline='') as csvfile:
+# create a new CSV file with the new header and rows
+with open(sys.argv[1].replace('.csv', '_new.csv'), 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(new_header)
     csvwriter.writerows(new_rows)
 
-print(f"Moved 'sys_id' column to first column in {filename} and saved the new file as {new_filename}")
+# close the CSV file
+csvfile.close()
+
+print("CSV transformation complete!")
