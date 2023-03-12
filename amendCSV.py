15c7455d5307fced7506f6a97910e7d7f36c2444
jnko266 <33516559+jnko266@users.noreply.github.com>
2023-03-12 23:41:42 +0000

    convert var names to camelCase

--- amendCSV.py
+++ amendCSV.py
@@ -12,29 +12,29 @@ if len(sys.argv) < 2:
 	sys.exit(1)
 
 # open the CSV file in read-only mode
-with open(sys.argv[1], 'r') as csvfile:
+with open(sys.argv[1], 'r') as csvFile:
     # read the CSV file and get the header row
-    csvreader = csv.reader(csvfile)
-    header = next(csvreader)
+    csvReader = csv.reader(csvFile)
+    header = next(csvReader)
     
 	# get the index of the sys_id column and create a new header with sys_id as the first column
-    sys_id_index = header.index("sys_id")
-    new_header = ["sys_id"] + [col for col in header if col != "sys_id"]
+    sysIdIndex = header.index("sys_id")
+    newHeader = ["sys_id"] + [col for col in header if col != "sys_id"]
     
 	# create a new list of rows with sys_id data as the first column
-    rows = [row for row in csvreader]
-    new_rows = [[row[sys_id_index]] + [col for i, col in enumerate(row) if i != sys_id_index] for row in rows]
+    rows = [row for row in csvReader]
+    newRows = [[row[sysIdIndex]] + [col for i, col in enumerate(row) if i != sysIdIndex] for row in rows]
     
 # close the CSV file
-csvfile.close()
+csvFile.close()
 
 # create a new CSV file with the new header and rows
-with open(sys.argv[1].replace('.csv', '_new.csv'), 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(new_header)
-    csvwriter.writerows(new_rows)
+with open(sys.argv[1].replace('.csv', '_new.csv'), 'w', newline='') as csvFile:
+    csvwriter = csv.writer(csvFile)
+    csvwriter.writerow(newHeader)
+    csvwriter.writerows(newRows)
 
 # close the CSV file
-csvfile.close()
+csvFile.close()
 
 print("CSV transformation complete!")
