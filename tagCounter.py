a95d746570575ec5ac4eb109843edd3d825c2747
jnko266 <33516559+jnko266@users.noreply.github.com>
2023-03-13 09:57:04 +0000

    allow for counting rows in CSV files

--- tagCounter.py
+++ tagCounter.py
@@ -3,7 +3,9 @@
 
 # import libraries
 import xml.etree.ElementTree as ET
+import csv
 import sys
+import os
 
 # check that a filename was passed as a parameter
 if len(sys.argv) < 2:
@@ -11,18 +13,34 @@ if len(sys.argv) < 2:
 	print("Usage: tagCounter.py <filename>")
 	sys.exit(1)
 
-# parse the XML file   
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
+# get file extension
+fileExt = os.path.splitext(sys.argv[1])[1]
 
-counts = {}
+if fileExt == ".xml":
+	# parse the XML file   
+	tree = ET.parse(sys.argv[1])
+	root = tree.getroot()
 
-for child in root:
-	if child.tag not in counts:
-		counts[child.tag] = 1
-	else:
-		counts[child.tag] += 1
+	# loop through child records and count them
+	counts = {}
+	for child in root:
+		if child.tag not in counts:
+			counts[child.tag] = 1
+		else:
+			counts[child.tag] += 1
 
-print("Counts:")
-for tag, count in counts.items():
-	print(f"{tag}: {count}")
+	# print result
+	print("Counts:")
+	for tag, count in counts.items():
+		print(f"{tag}: {count}")
+
+
+elif fileExt == ".csv":
+	# open the CSV file in read-only mode and count rows (excluding header)
+	with open(sys.argv[1], 'r') as f:
+		reader = csv.reader(f)
+		header = next(reader)
+		numRows = sum(1 for row in reader)
+
+	# print result
+	print(f"Number of rows (excluding header): {numRows}")
